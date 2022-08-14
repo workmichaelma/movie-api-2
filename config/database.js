203492d15cba81f4ed82098dda357904e63d9457
Michael <work.michael.ma@gmail.com>
2022-08-15 02:35:21 +0800

    Add database connection

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const config = {};
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host:
+        config.host || "movie-backend.csr0jb4deere.us-east-1.rds.amazonaws.com",
+      port: config.port || "5432",
+      database: env.NODE_ENV === "production" ? "movie" : "movie_dev",
+      user: config.user || "michaelma",
+      password: config.password || "footballwork",
       ssl: {
         rejectUnauthorized: false,
       },
