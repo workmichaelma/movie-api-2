989238d84d29b8a84f70a7dec070f08443aeaa85
Michael <work.michael.ma@gmail.com>
2022-08-24 23:50:58 +0800

    Enable cronjob in prod server setup

--- app/config/env/production/server.js
+++ app/config/env/production/server.js
@@ -1,3 +1,4 @@
+const cronTasks = require("./cron-tasks");
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
@@ -5,6 +6,7 @@ module.exports = ({ env }) => ({
     keys: env.array("APP_KEYS"),
   },
   cron: {
-    enabled: false,
+    enabled: true,
+    tasks: cronTasks,
   },
 });
